6bc74c539d4e657729c2b204408029acfd894809
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-04-11 00:41:47 +0300

    Implement Todo component basic functionality

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import "./Todo.css";
 
 class Todo extends React.Component {
+    state = {isCompleted: this.props.isCompleted}
+
+    handleComplete = evt => {
+        this.setState(st => ({
+            isCompleted: !st.isCompleted
+        }));
+    }
+
+    handleRemove = () => {
+        this.props.removeHandler(this.props.id);
+    }
+
     render() {
+        const {id, task} = this.props;
+        const {isCompleted} = this.state;
+
         return (
             <div>
-                Todo
+                <input type="checkbox" name="" id={id} checked={isCompleted} onChange={this.handleComplete}/>
+                <label htmlFor={id}>{task}</label>
+                <button onClick={this.handleRemove}>X</button>
             </div>
         );
     }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,11 +4,35 @@ import NewTodoForm from "./NewTodoForm";
 import "./TodoList.css";
 
 class TodoList extends React.Component {
+    // state = {todos: []}
+
+    // Mock state
+    state = {todos: [
+        {id: 0, task: "Do something", isCompleted: false},
+        {id: 1, task: "Do again", isCompleted: true},
+    ]}
+
+    removeTodo = id => {
+        this.setState(st => ({
+            todos: st.todos.filter(todo => (todo.id !== id))
+        }));
+    }
+
     render() {
+        const {todos} = this.state;
+
         return (
             <div>
-                TodoList
-                <Todo />
+                <h1>TodoList</h1>
+                {todos.map(({id, task, isCompleted}) => (
+                    <Todo 
+                        key={id}
+                        id={id}
+                        task={task}
+                        isCompleted={isCompleted}
+                        removeHandler={this.removeTodo}
+                    />
+                ))}
                 <NewTodoForm />
             </div>
         );
