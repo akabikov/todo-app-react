8ae9a825a986d2d414844823978e332f042bd749
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-05-02 16:37:12 +0300

    Updates

---
+++ precache-manifest.32f58ec2c2505d6b2753b05b404af0f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f8c10a855955a0124a2b9b5764c7e0d",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "b1464c9b9f0df29e986b",
+    "url": "/todo-app-react/static/css/main.246c77db.chunk.css"
+  },
+  {
+    "revision": "dca58b964e0f9c13bcf7",
+    "url": "/todo-app-react/static/js/2.aef76f71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-react/static/js/2.aef76f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1464c9b9f0df29e986b",
+    "url": "/todo-app-react/static/js/main.fe1ab396.chunk.js"
+  },
+  {
+    "revision": "df71405ded007a1b7ad7",
+    "url": "/todo-app-react/static/js/runtime-main.3ac87479.js"
+  }
+]);
